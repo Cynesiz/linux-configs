81547d0a05834cc68e8d3769de0421dd54721292
Riemannian <admin@cynicalsystems.com>
2016-06-24 12:29:32 -0400

    Update startup-pkgs.sh

--- startup-pkgs.sh
+++ startup-pkgs.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get upgrade
 apt-get dist-upgrade
 
-apt-get install build-essential sudo nano iptraf htop nmap gnupg dnsutils iptables-persistent fail2ban 
+apt-get install build-essential sudo nano iptraf htop nmap gnupg dnsutils iptables-persistent fail2ban haveged rng-tools
