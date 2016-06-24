dd23d9d6d9bc46729f0631f409d2ade15c1760c6
Riemannian <admin@cynicalsystems.com>
2016-06-24 06:57:02 -0400

    Create startup-pkgs.sh

---
+++ startup-pkgs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+
+apt-get install aptitiude
+aptitiude install build-essential sudo nano iptraf htop nmap gnupg dnsutils iptables-persistent fail2ban 
