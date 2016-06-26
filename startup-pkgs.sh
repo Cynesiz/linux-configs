8c085aaa443c4b5c599e8a8c62f8dca2ef74b20f
Riemannian <admin@cynicalsystems.com>
2016-06-26 06:15:30 -0400

    Update startup-pkgs.sh

--- startup-pkgs.sh
+++ startup-pkgs.sh
@@ -6,4 +6,10 @@ apt-get update
 apt-get upgrade
 apt-get dist-upgrade
 
-apt-get install build-essential sudo nano iptraf htop nmap gnupg dnsutils iptables-persistent fail2ban haveged rng-tools
+PACKAGES="
+build-essential sudo nano iptraf htop nmap gnupg dnsutils 
+iptables-persistent fail2ban haveged rng-tools acpid apt-file bind9-host bzip2
+curl emacs24-nox htop nmon ntp rsync slurm tcpdump unzip vim-nox
+"
+apt-get -y install --without-recommends $PACKAGES
+
