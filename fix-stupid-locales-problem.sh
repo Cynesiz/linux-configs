7f18197458b129e1f95da8d0ddf555d3f097925e
(◣_◢) <admin@cynicalsystems.com>
2016-06-27 05:14:41 -0400

    Added more stuff

--- fix-stupid-locales-problem.sh
+++ fix-stupid-locales-problem.sh
@@ -3,15 +3,22 @@
 # One of these fucking commands will fix it!
 # Tested and working on debian :)
 
+# Purge/Install Stuff
+sudo apt-get purge locales
+sudo apt-get install locales
+sudo apt-get install language-pack-en-base
+
+# Write stuff
 sudo sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
-echo "export LANGUAGE=en_US.UTF-8
+sudo sh -c "LC_ALL=en_EN.UTF-8" >> /etc/default/locale
+sudo sh -c "export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8">>~/.bash_profile
 source ~/.bash_profile
-sudo apt-get purge locales
-sudo apt-get install locales
-sudo apt-get install language-pack-en-base  
+
+# Configure stuff
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 sudo locale-gen "en_US.UTF-8"
+sudo locale-gen en_EN.UTF-8
 sudo dpkg-reconfigure locales
 
