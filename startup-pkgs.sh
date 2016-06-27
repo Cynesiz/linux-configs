e6a7a0f4a8604abe0b94dff7189300b2b3e6b4f4
(◣_◢) <admin@cynicalsystems.com>
2016-06-26 22:02:49 -0400

    Update startup-pkgs.sh

--- startup-pkgs.sh
+++ startup-pkgs.sh
@@ -9,5 +9,5 @@ build-essential sudo nano iptraf htop nmap gnupg dnsutils
 iptables-persistent fail2ban haveged rng-tools acpid apt-file bind9-host bzip2
 curl emacs24-nox htop nmon ntp rsync slurm tcpdump unzip vim-nox
 "
-apt-get -y install --without-recommends $PACKAGES
+apt-get -y install $PACKAGES
 
