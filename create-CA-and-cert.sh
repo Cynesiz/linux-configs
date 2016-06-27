0b7809a7812121ff152a7950e3f1f2a690e34d96
(◣_◢) <admin@cynicalsystems.com>
2016-06-27 11:34:59 -0400

    Update create-CA-and-cert.sh

--- create-CA-and-cert.sh
+++ create-CA-and-cert.sh
@@ -23,15 +23,15 @@ openssl x509 -sha512 -req -days ${daysvalid} -in "${domain}.csr" -CA ca.crt -CAk
 
 if [ $# -eq 0 ]
 then
-while true; do
     read -p "What domain do you want the cert for?  (example.com)" domain
     read -p "How many key bits? (2048, 4096, etc)" keybits
     read -p "How many days should cert be valid? (365)" daysvalid
     makeca
     makecsr
     makecert
-done
+    exit 0
 else
 dochoice $1
+exit 0
 fi
 
