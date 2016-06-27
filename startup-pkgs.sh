0503062cf56ef0a2317bc8d4a2dfeab2d9255ed8
(◣_◢) <admin@cynicalsystems.com>
2016-06-26 22:09:39 -0400

    Update startup-pkgs.sh

--- startup-pkgs.sh
+++ startup-pkgs.sh
@@ -5,9 +5,9 @@ apt-get upgrade
 apt-get dist-upgrade
 
 PACKAGES="
-build-essential sudo nano iptraf htop nmap gnupg dnsutils 
-iptables-persistent fail2ban haveged rng-tools acpid apt-file bind9-host bzip2
-curl emacs24-nox htop nmon ntp rsync slurm tcpdump unzip vim-nox
+git sudo nano iptraf htop nmap gnupg dnsutils 
+iptables-persistent haveged rng-tools acpid apt-file bzip2
+curl htop nmon ntp rsync slurm tcpdump unzip
 "
 apt-get -y install $PACKAGES
 
