538df84d4ca0a10dcfa77ad5abf609d0c54116e8
(◣_◢) <admin@cynicalsystems.com>
2016-06-27 11:25:59 -0400

    Update create-CA-and-cert.sh

--- create-CA-and-cert.sh
+++ create-CA-and-cert.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-
-
-openssl genrsa -aes256 -out ca.key 4096
+function makeca () 
+{
+openssl genrsa -aes256 -out ca.key ${keybits}
 chmod 400 ca.key
+}
 
-openssl req -new -x509 -days 3650 -key ca.key -sha256 -extensions v3_ca -out ca.crt
+function makecsr ()
+{
+openssl req -new -x509 -days ${daysvalid} -key ca.key -sha512 -extensions v3_ca -out ca.crt
 chmod 400 ca.crt
+}
 
-openssl genrsa -out example.com.key 4096
-chmod 400 example.com.key
+function makecert ()
+{
+openssl genrsa -out "${domain}.key" $[keybits]
+chmod 400 "${domain}.key"
+openssl req -sha512 -new -key "${domain}.key" -out "${domain}.csr"
+openssl x509 -sha512 -req -days ${daysvalid} -in "${domain}.csr" -CA ca.crt -CAkey ca.key -CAcreateserial -out "${domain}.crt"
+}
 
-openssl req -sha256 -new -key example.com.key -out example.com.csr
-openssl x509 -sha256 -req -days 365 -in example.com.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out example.com.crt
+
+if [ $# -eq 0 ]
+then
+while true; do
+    read -p "What domain do you want the cert for?  (example.com)" domain
+    read -p "How many key bits? (2048, 4096, etc)" keybits
+    read -p "How many days should cert be valid? (365)" daysvalid
+    makeca
+    makecsr
+    makecert
+done
+else
+dochoice $1
+fi
+
