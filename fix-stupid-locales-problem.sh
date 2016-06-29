46c20bb0f42cd7bc42eb8204bb84a45395a7f4e6
(◣_◢) <admin@cynicalsystems.com>
2016-06-29 05:04:25 -0400

    fixed shit

--- fix-stupid-locales-problem.sh
+++ fix-stupid-locales-problem.sh
@@ -10,18 +10,17 @@ sudo apt-get install language-pack-en-base
 
 # Write stuff
 sudo sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
-sudo sh -c "LC_ALL=en_EN.UTF-8" >> /etc/default/locale
+sudo sh -c "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 sudo sh -c "export LANGUAGE=\"en_US.UTF-8\"
 export LANG=\"en_US.UTF-8\"
 export LC_ALL=\"en_US.UTF-8\"">>~/.bash_profile
-echo "LANG=\"en_AU.UTF-8\"\nLANGUAGE=\"en_AU:en\"" >> ~/.profile
+echo "LANG=\"en_US.UTF-8\"\nLANGUAGE=\"en_US:en\"" >> ~/.profile
 source ~/.bash_profile
 source ~/.profile
 
 # Configure stuff
 
 sudo locale-gen "en_US.UTF-8"
-sudo locale-gen en_EN.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 
 sudo dpkg-reconfigure locales
