1c26904b301a7a27975042acf6afa794419df0e0
(◣_◢) <admin@cynicalsystems.com>
2016-07-01 10:00:43 -0400

    Update essentials.sh

--- essentials.sh
+++ essentials.sh
@@ -19,9 +19,9 @@ apt-get dist-upgrade
 while true; do
     read -p "\n[?] Choose: server / standard / virtual / quit :  " choice
     case $choice in
-        [server]* ) update; apt-get -y install "git sudo nano wget links iptraf htop nmap gnupg dnsutils iptables-persistent haveged rng-tools acpid apt-file bzip2 curl htop nmon ntp rsync tcpdump unzip"; break;;
-        [standard]* ) update; apt-get -y install "git sudo nano wget links iptraf htop gnupg dnsutils haveged rng-tools acpid bzip2 curl htop rsync tcpdump unzip"; break;;
-        [virtual]* ) update; apt-get -y install "git sudo nano wget links iptraf htop dnsutils acpid unzip curl tcpdump rng-utils"; break;;
+        [server]* ) update; apt-get -y install "git sudo nano wget links iptraf htop nmap gnupg dnsutils iptables-persistent haveged rng-tools acpid apt-file bzip2 curl htop nmon ntp rsync tcpdump unzip ca-certificates"; break;;
+        [standard]* ) update; apt-get -y install "git sudo nano wget links iptraf htop gnupg dnsutils haveged rng-tools acpid bzip2 curl htop rsync tcpdump unzip ca-certificates"; break;;
+        [virtual]* ) update; apt-get -y install "git sudo nano wget links iptraf htop dnsutils acpid unzip curl tcpdump rng-utils ca-certificates"; break;;
         [quit]* ) exit;;
         * ) read -p "\n[?] Choose: server / standard / virtual / quit :  " choice
     esac
