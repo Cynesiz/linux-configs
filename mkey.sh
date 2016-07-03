09c170c304e4c250ef9a4ba9a9af789f76b94c89
(◣_◢) <admin@cynicalsystems.com>
2016-07-03 02:34:13 -0400

    Create mkey.sh

---
+++ mkey.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Shortcut functions/alias for ssh-keygen
+
+# Generate Host Keys
+
+function hkeygen ()
+{
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to generate host keys!" 2>&1
+  exit 1
+else
+  rm -rf /etc/ssh/ssh_host*
+  ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
+  ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
+fi
+}
+
+# Generate Client Keys
+
+function ckeygen ()
+{
+  printf "%b" "\n\e[30;48;5;82m Generating Client SSH Keys \e[0m \n" 
+  printf "%b" "\n\e[40;38;5;82m Output Directory: $(echo ~/.ssh) \e[0m \n"
+  ssh-keygen -t ed25519 -f $(echo ~/.ssh/$1_ed25519)
+  ssh-keygen -t rsa -b 4096 -f $(echo ~/.ssh/$1_rsa)
+  printf "%b" "\n\e[40;38;5;82m Public keys are as follows... \e[0m \n"
+  printf "%b" "\n\e[40;38;5;82m ED25519 \e[0m \n"
+  cat $(echo ~/.ssh/$1_ed25519.pub)
+  printf "%b" "\n\e[40;38;5;82m RSA4096 \e[0m \n"
+  cat $(echo ~/.ssh/$1_rsa.pub)
+  echo "\n"
+  exit 0
+}
+
+# Handle CLI arguments
+
+if [ $# -eq 0 ]; 
+then
+    echo "Usage: $0 <host/client> <name (client only, optional)> "
+    exit 1
+else
+  if [ $1 == 'host' ]; 
+  then
+    hkeygen
+  else
+    if [ $2 -eq 0 ]; 
+    then
+      ckeygen id
+    else
+      ckeygen $2
+    fi
+  fi
+fi
